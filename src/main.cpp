6e3679308bbedabcca2abc0398871dcde22b48b5
Nathan Seymour <nathan@seymour.global>
2024-05-24 18:51:26 -0500

    add sfml integration with basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 #include "MathFunction.h"
 
 int main()
@@ -11,6 +12,27 @@ int main()
     double value = eq.Evaluate(context);
 
     std::cout << value << std::endl;
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+
+        window.display();
+    }
 
     return 0;
 }
