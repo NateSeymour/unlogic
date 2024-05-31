4c9f2c96ec3698ba7a69039a43d64b7d2924f0cd
Nathan Seymour <nathan@seymour.global>
2024-05-31 12:22:45 -0500

    update graph test

--- test/GraphRenderer.test.cpp
+++ test/GraphRenderer.test.cpp
@@ -1,17 +1,20 @@
 #include <gtest/gtest.h>
+#include <fstream>
+#include <iostream>
 #include "Graph.h"
 
 TEST(GraphRenderer, QuadraticImageGeneration)
 {
     // Generate image
+    Graph graph({"f[x] = x^2;"});
+
     sf::RenderTexture texture;
     texture.create(250, 250);
 
-    GraphRenderer renderer("f[x] = x^2;");
-
-    renderer.Draw(texture);
+    texture.clear(sf::Color::White);
+    texture.draw(graph);
 
     sf::Image image = texture.getTexture().copyToImage();
     std::vector<sf::Uint8> buffer;
-    image.saveToMemory(buffer, "jpg");
+    image.saveToMemory(buffer, "png");
 }
