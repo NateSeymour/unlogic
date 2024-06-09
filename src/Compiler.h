7d29f3bf7c4b075611eff2f549c18ae49c18fd81
Nathan Seymour <nathan@seymour.global>
2024-06-09 16:32:10 -0500

    basic JIT compiler config

--- src/Compiler.h
+++ src/Compiler.h
@@ -5,11 +5,37 @@
 #ifndef UNLOGIC_COMPILER_H
 #define UNLOGIC_COMPILER_H
 
+#include <string>
+#include <llvm/ExecutionEngine/Orc/Core.h>
+#include <llvm/ExecutionEngine/Orc/ExecutorProcessControl.h>
+#include <llvm/ExecutionEngine/Orc/IRCompileLayer.h>
+#include <llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#include "Parser.h"
+
 namespace unlogic
 {
-    class Compiler
+    class Callable
     {
+        llvm::orc::ExecutionSession execution_session;
+        llvm::orc::RTDyldObjectLinkingLayer object_layer;
+        llvm::orc::IRCompileLayer compile_layer;
 
+        Parser parser;
+    public:
+        Callable(std::string const &unlogic_function) :
+            parser(unlogic_function),
+            execution_session(std::move(*llvm::orc::SelfExecutorProcessControl::Create())),
+            object_layer(this->execution_session, []() { return std::make_unique<llvm::SectionMemoryManager>(); }),
+            compile_layer(this->execution_session, )
+        {
+            auto prototype = this->parser.ParseFunctionDefinition();
+        }
+
+        ~Callable()
+        {
+            this->execution_session.endSession();
+        }
     };
 }
 
