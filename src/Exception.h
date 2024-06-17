3c9a4d4a64d3157b9f678dea30dd388a1ae4f979
Nathan Seymour <nathan@seymour.global>
2024-06-17 09:36:54 -0400

    remove unused tokenviewer

--- src/Exception.h
+++ src/Exception.h
@@ -2,29 +2,7 @@
 #define UNLOGIC_EXCEPTION_H
 
 #include <stdexcept>
-#include "TokenViewer.h"
 
-namespace unlogic
-{
-    class TokenException : public std::exception
-    {
-        TokenViewer const &tv_;
-
-    public:
-        enum TokenExceptionType
-        {
-            UnknownSymbol
-        };
-
-        TokenExceptionType type;
-
-        char const *what() const noexcept override
-        {
-            return "an issue occurred while tokenizing the input.";
-        }
-
-        TokenException(TokenViewer const &tv, TokenExceptionType type = UnknownSymbol) : tv_(tv), type(type) {}
-    };
-}
+namespace unlogic {}
 
 #endif //UNLOGIC_EXCEPTION_H
