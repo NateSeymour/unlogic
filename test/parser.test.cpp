d534e4924cd1f3ed10840a4b528c8eda36e5faa3
Nathan Seymour <nathan@seymour.global>
2024-07-13 17:58:11 -0500

    basic isolated test setup for parser

---
+++ test/parser.test.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+//#include "parser/Lex.h"
+
+/*
+enum class TerminalType
+{
+    NUMBER,
+};
+
+unlogic::Lex<TerminalType> lex;
+
+unlogic::Terminal<TerminalType, TerminalType::NUMBER, R"(\-?\d*\.\d*)", double> number(lex, [](auto position) {
+    return 0.0;
+});
+*/
+
+TEST(Lex, Basic)
+{
+
+}
