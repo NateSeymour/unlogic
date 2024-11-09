406fafdf97a336ba9fa094665c8e60154a6bb443
Nathan Seymour <nathan@seymour.global>
2024-11-09 13:28:22 -0600

    initial qt setup;

--- src/calculator/main.cpp
+++ src/calculator/main.cpp
@@ -1,35 +1,13 @@
-#include <thread>
-#include <gtkmm.h>
-#include "main.h"
-#include "compiler/Compiler.h"
-#include "ui/Window.h"
+#include <QApplication>
+#include <QtWidgets/QtWidgets>
 
-/*
- * Data structures:
- * - Worker thread that upon notification will do compilation tasks
- * - Worker thread that will take care of syntax highlighting
- * - Global storage for graphed objects and general results
- * - Dispatchers for synchronization
- */
-
-void init_compiler_runtime()
+int qMain(int argc, char **argv)
 {
-    unlogic::Compiler::InitializeGlobalCompilerRuntime();
+    QApplication app(argc, argv);
 
-    unlogic::compiler_runtime_avail = true;
-    unlogic::compiler_runtime_avail.notify_all();
-}
+    QWidget window;
+    window.resize(100, 100);
+    window.show();
 
-int main(int argc, char *argv[])
-{
-    std::cout << "[MAIN] Initializing compiler runtime..." << std::endl;
-    std::thread init_thread(init_compiler_runtime);
-
-    std::cout << "[MAIN] Creating application..." << std::endl;
-    auto app = Gtk::Application::create("global.seymour.unlogic");
-    int ret = app->make_window_and_run<unlogic::Window>(argc, argv);
-
-    init_thread.join();
-
-    return ret;
+    return app.exec();
 }
--- src/calculator/ui/Window.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by nathan on 7/2/24.
-//
-
-#include "Window.h"
-
-namespace unlogic {
-} // unlogic
