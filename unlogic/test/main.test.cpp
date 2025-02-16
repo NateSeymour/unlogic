2b8a1a53c8f640d8387b303ba5dbb651b6316d9e
Nathan Seymour <nathan@seymour.global>
2025-02-16 10:48:16 -0600

    fix broken test;

--- unlogic/test/main.test.cpp
+++ unlogic/test/main.test.cpp
@@ -1,11 +1,8 @@
 #include <gtest/gtest.h>
-#include "compiler/Compiler.h"
 
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
-
-    unlogic::Compiler::InitializeGlobalCompilerRuntime();
 
     return RUN_ALL_TESTS();
 }
